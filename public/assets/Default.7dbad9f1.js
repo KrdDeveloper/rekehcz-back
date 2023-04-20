cb402a14aa4ab1af238c0bfcd18d7ebd9c6b4e6c
ropsnap <ropsnap9@gmail.com>
2023-04-20 03:44:48 -0300

    de

---
+++ public/assets/Default.7dbad9f1.js
@@ -0,0 +1,1 @@
+import{r as n,o as e,c as t,w as a,a as r,V as p,b as l}from"./index.19693a2e.js";const u={__name:"View",setup(o){return(s,_)=>{const c=n("router-view");return e(),t(p,{class:"pa-6",style:{border:"1px solid lightgrey"}},{default:a(()=>[r(c)]),_:1})}}},m={__name:"Default",setup(o){return(s,_)=>(e(),t(l,null,{default:a(()=>[r(u)]),_:1}))}};export{m as default};
