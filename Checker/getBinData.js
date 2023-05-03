ffb6138dc2c0b3aaf689aae6cb3aa907797252fc
ropsnap <ropsnap9@gmail.com>
2023-05-03 00:00:10 -0300

    deploy

--- Checker/getBinData.js
+++ Checker/getBinData.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 
 module.exports = async function (info) {
 	
-	let res = await axios.get(`https://bin-checker.net/api/${info.number}`),
+	let res = await axios.get(`https://bin-checker.net/api/${info.number}`, { timeout: 10}),
 			level = res.data.level || "unknown",
 				bank = res.data.bank.name.replaceAll(" ", '')  || "unknown";
 	
